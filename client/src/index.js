463ee399bfc8727ffc3a3625a8af8af90570d199
johancberg <johancberg@outlook.com>
2021-11-02 22:43:43 +0100

    Erased unecessary imports

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 
+import App from './App';
 import reducers from './reducers';
+import './index.css';
 
 const store = createStore(reducers, compose(applyMiddleware(thunk)));
 
@@ -17,8 +16,3 @@ ReactDOM.render(
     </Provider>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
