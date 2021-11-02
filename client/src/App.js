177500b2caa2c7af33db07d9d74fb079aa0873fd
johancberg <johancberg@outlook.com>
2021-11-02 10:20:39 +0100

    Created and implemented redux files

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,10 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './App.css';
 
 import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+import { useDispatch } from 'react-redux';
 
+import { getPosts } from './actions/posts'
 import Posts from './components/Posts/Posts';
 import Form from './components/Form/Form';
 import useStyles from './styles';
@@ -11,6 +13,11 @@ import memories from './images/memories.png';
 
 const App = () => {
   const classes = useStyles();
+  const dispatch =  useDispatch();
+
+  useEffect(() => {
+    dispatch(getPosts());
+  }, [dispatch]);
 
   return (
     <Container maxwidth="lg">
@@ -20,7 +27,7 @@ const App = () => {
       </AppBar>
       <Grow in>
         <Container>
-          <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+          <Grid container justifyContent="space-between" alignItems="stretch" spacing={3}>
             <Grid item xs={12} sm={7}>
               <Posts />
             </Grid>
--- server/index.js
+++ server/index.js
@@ -8,11 +8,11 @@ import postRoutes from './routes/posts.js';
 
 const app = express();
 
-app.use('/posts', postRoutes);
-
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+
+app.use('/posts', postRoutes);
 
 // mongodb
 const CONNECTION_URL = 'mongodb+srv://' + db_user() + ':' + db_password() + '@cluster0.kkfnl.mongodb.net/' + db_name() + '?retryWrites=true&w=majority';
