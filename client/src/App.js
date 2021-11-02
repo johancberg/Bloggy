99a2e127294d73a313a6fffdb4ef29735345eeaf
johancberg <johancberg@outlook.com>
2021-11-02 22:47:54 +0100

    Changed the frontend name on App

--- client/src/App.js
+++ client/src/App.js
@@ -22,7 +22,7 @@ const App = () => {
   return (
     <Container maxwidth="lg">
       <AppBar className={classes.appBar} position="static" color="inherit">
-        <Typography className={classes.header} variant="h2" align="center">Memories</Typography>
+        <Typography className={classes.header} variant="h2" align="center">Bloggy</Typography>
         <img className={classes.image} src={memories} alt="memories" height="60" />
       </AppBar>
       <Grow in>
