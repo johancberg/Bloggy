eaf94abcd587f9c85c89d8d8769b2629765eb1e4
johancberg <johancberg@outlook.com>
2021-11-15 23:43:57 +0100

    Revert "Changed the unpushed credentials.js to .env"
    This reverts commit e568cd242a73d4cb50292c49f3b2aa6acbbf21dc.

--- server/index.js
+++ server/index.js
@@ -2,13 +2,11 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
-import dotenv from 'dotenv';
-//import { db_user, db_password, db_name } from './credentials.js';
+import { db_user, db_password, db_name } from './credentials.js';
 
 import postRoutes from './routes/posts.js';
 
 const app = express();
-dotenv.config();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
@@ -17,9 +15,9 @@ app.use(cors());
 app.use('/posts', postRoutes);
 
 // mongodb
-//const CONNECTION_URL = 'mongodb+srv://' + db_user() + ':' + db_password() + '@cluster0.kkfnl.mongodb.net/' + db_name() + '?retryWrites=true&w=majority';
+const CONNECTION_URL = 'mongodb+srv://' + db_user() + ':' + db_password() + '@cluster0.kkfnl.mongodb.net/' + db_name() + '?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
     .catch((error) => console.log(error.message));
