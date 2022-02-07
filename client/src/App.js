bbebc4e998f3af1fbc838e6b780ab1f6fe541b82
johancberg <johancberg@outlook.com>
2022-02-07 19:59:08 +0100

    Began working on a pagination layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,27 @@
 import React from 'react'
 import { Container } from '@material-ui/core';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Redirect, Routes, Route } from 'react-router-dom';
 
 import Navbar from './components/Navbar/Navbar';
 import Home from './components/Home/Home';
 import Auth from './components/Auth/Auth';
+import PostDetails from './components/PostDetails/PostDetails';
 
 const App = () => {
+
+  /*
+  Include later below Routes
+  <Route path='/' exact element={() => <Redirect to="/posts" />} />
+  */
+
   return (
     <BrowserRouter>
-      <Container maxwidth="lg">
+      <Container maxwidth="xl">
         <Navbar />
         <Routes>
-          <Route path='/' exact element={<Home/>} />
+          <Route path='/posts' exact element={<Home/>} />
+          <Route path='/posts/search' exact element={<Home/>} />
+          <Route path='/posts/:id' exact element={<PostDetails/>} />
           <Route path='/auth' exact element={<Auth/>} />
         </Routes>
       </Container>
--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import { Container, Grow, Grid } from '@material-ui/core';
+import { Container, Grow, Grid, Paper } from '@material-ui/core';
 import { useDispatch } from 'react-redux';
 import { getPosts } from '../../actions/posts'
 
 import Posts from '../Posts/Posts';
 import Form from '../Form/Form';
+import Paginate from '../Paginate';
 
 const Home = () => {
   const [currentId, setCurrentId] = useState(null);
@@ -24,6 +25,9 @@ const Home = () => {
           </Grid>
           <Grid item xs={12} sm={4}>
             <Form currentId={currentId} setCurrentId={setCurrentId} />
+            <Paper elevation={6} >
+              <Paginate />
+            </Paper>
           </Grid>
         </Grid>
       </Container>
