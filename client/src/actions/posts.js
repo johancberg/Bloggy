6fbbab5256070edbaa329db966dc262250bb1b72
johancberg <johancberg@outlook.com>
2022-04-16 16:00:18 +0200

    Made the backend handle the search logic

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -12,9 +12,9 @@ export const getPosts = () => async (dispatch) => {
     }
 }
 
-export const getPostsBySearch = () => async (dispatch) => {
+export const getPostsBySearch = (searchQuery) => async (dispatch) => {
     try {
-        const { data } = await api.fetchPostsBySearch();
+        const { data : { data } } = await api.fetchPostsBySearch(searchQuery);
         console.log(data);
     } catch (error) {
         console.log(error);
