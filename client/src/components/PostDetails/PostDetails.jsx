e30f81d178c8a596067840b93570a8d30263d053
johancberg <johancberg@outlook.com>
2022-04-19 00:27:00 +0200

    Added logic to recommend posts

--- client/src/components/PostDetails/PostDetails.jsx
+++ client/src/components/PostDetails/PostDetails.jsx
@@ -3,7 +3,7 @@ import { Paper, Typography, CircularProgress, Divider } from '@material-ui/core'
 import { useDispatch, useSelector } from 'react-redux';
 import moment from 'moment';
 import { useParams, useNavigate } from 'react-router-dom';
-import { getPost } from '../../actions/posts';
+import { getPost, getPostsBySearch } from '../../actions/posts';
 
 import useStyles from './styles';
 
@@ -18,6 +18,12 @@ const PostDetails = () => {
         dispatch(getPost(id));
     }, [id, dispatch]);
 
+    useEffect(() => {
+        if (post) {
+            dispatch(getPostsBySearch({ tags: post?.tags.join(',') }));
+        }
+    }, [post, dispatch]);
+
     if (!post) return null;
 
     if (isLoading) {
@@ -26,6 +32,12 @@ const PostDetails = () => {
                 <CircularProgress size="7em" />
             </Paper>
         )
+    }
+
+    const recommendedPosts = posts.filter(({ _id }) => _id !== post._id);
+
+    const openPost = (_id) => {
+        navigate(`/posts/${_id}`);
     }
 
     return (
@@ -47,7 +59,23 @@ const PostDetails = () => {
                     <img className={classes.media} src={post.selectedFile || 'https://user-images.githubusercontent.com/194400/49531010-48dad180-f8b1-11e8-8d89-1e61320e1d82.png'} alt={post.title} />
                 </div>
             </div>
-
+            { recommendedPosts.length && (
+                <div className={classes.section}>
+                    <Typography gutterBottom variant="h5">You might also like:</Typography>
+                    <Divider />
+                    <div className={classes.recommendedPosts}>
+                        {recommendedPosts.map(({ title, message, name, likes, selectedFile, _id }) => (
+                            <div style={{ margin: '20px', cursor: 'pointer' }} onClick={() => openPost(_id)} key={_id}>
+                                <Typography gutterBottom variant="h6">{title}</Typography>
+                                <Typography gutterBottom variant="subtitle2">{name}</Typography>
+                                <Typography gutterBottom variant="subtitle2">{message}</Typography>
+                                <Typography gutterBottom variant="subtitle1">Likes: {likes.length}</Typography>
+                                <img src={selectedFile} alt="recommended posts" width="200px" />
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            ) }
         </Paper>
     );
 }
