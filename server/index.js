6f1d1704532f7955e0368491ee035db85b68e6d0
johancberg <johancberg@outlook.com>
2022-05-15 23:07:50 +0200

    Uninstalled an unnecessary package

--- server/index.js
+++ server/index.js
@@ -1,5 +1,4 @@
 import express from 'express';
-import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import { db_user, db_password, db_name } from './credentials.js';
@@ -9,8 +8,8 @@ import userRoutes from './routes/users.js';
 
 const app = express();
 
-app.use(bodyParser.json({ limit: "30mb", extended: true }));
-app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(express.json({ limit: '30mb', extended: true }))
+app.use(express.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 
 app.use('/posts', postRoutes);
