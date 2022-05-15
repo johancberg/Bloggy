ea25ddd5cfc6f301933a4e18cc5c75d2f23b2a46
johancberg <johancberg@outlook.com>
2022-05-15 23:27:34 +0200

    Changed the CORS policy slightly

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(express.json({ limit: '30mb', extended: true }))
 app.use(express.urlencoded({ limit: '30mb', extended: true }))
-app.use(cors());
+app.use(cors({ origin: "*" }));
 
 app.use('/posts', postRoutes);
 app.use('/user', userRoutes);
