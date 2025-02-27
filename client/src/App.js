622ebda6a682c858402b7ea3b0965e7024ab8a82
johancberg <johancberg@outlook.com>
2025-02-28 00:37:38 +0100

    Implement ThemeProvider and a temporary theme

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Container } from '@mui/material';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { BrowserRouter, Navigate, Routes, Route } from 'react-router-dom';
 
 import Navbar from './components/Navbar/Navbar';
@@ -8,21 +9,33 @@ import Auth from './components/Auth/Auth';
 import PostDetails from './components/PostDetails/PostDetails';
 
 const App = () => {
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: '#f44336',
+      },
+      secondary: {
+        main: '#3f51b5',
+      },
+    },
+  });
   const user = JSON.parse(localStorage.getItem('profile'));
 
   return (
-    <BrowserRouter>
-      <Container maxwidth="xl">
-        <Navbar />
-        <Routes>
-          <Route path='/' exact element={<Navigate to="/posts" />} />
-          <Route path='/posts' exact element={<Home/>} />
-          <Route path='/posts/search' exact element={<Home/>} />
-          <Route path='/posts/:id' exact element={<PostDetails/>} />
-          <Route path='/auth' exact element={!user ? <Auth/> : <Navigate to="/posts" />} />
-        </Routes>
-      </Container>
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <Container maxwidth="xl">
+          <Navbar />
+          <Routes>
+            <Route path='/' exact element={<Navigate to="/posts" />} />
+            <Route path='/posts' exact element={<Home/>} />
+            <Route path='/posts/search' exact element={<Home/>} />
+            <Route path='/posts/:id' exact element={<PostDetails/>} />
+            <Route path='/auth' exact element={!user ? <Auth/> : <Navigate to="/posts" />} />
+          </Routes>
+        </Container>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
